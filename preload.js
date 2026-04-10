5c33b6fd43514981230888ec0b167ed9a5f582d7
linntiffanyyy <linn.tiffanyyy@gmail.com>
2026-04-10 00:16:59 -0400

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  minimize: () => ipcRenderer.send("window-minimize"),
-  close: () => ipcRenderer.send("window-close")
-});
